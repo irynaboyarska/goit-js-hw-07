93c6f70b78e48f0099cd75c7921f7ee5d6b6a6d7
Iryna Boyarska <boyarskairyna32@gmail.com>
2026-02-17 14:34:02 +0200

    done task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(`.login-form`);
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault();
 
+    const formData = new FormData(form);
+    const email = formData.get(`email`).trim();
+    const password = formData.get(`password`).trim();
+
+    if (email === "" || password === "") {
+        alert(`All form fields must be filled in`);
+        return;
+    }
+
+    const obj = {
+        email,
+        password,
+    }
+    console.log(obj);
+    
+    form.reset();
+})
